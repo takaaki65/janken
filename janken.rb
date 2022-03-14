6f619b8ae96db68f5b639e9db92fa433850677c5
takaaki65 <76176467+takaaki65@users.noreply.github.com>
2022-03-14 14:36:15 +0900

    Add files via upload
    相手が出した手をグー、チョキ、パーで表示。
    ジャンケンで0~2以外の数字を入力すると終了するように変更。
    インデントを半角スペース２つに変更。

--- janken.rb
+++ janken.rb
@@ -2,73 +2,75 @@ puts "ジャンケン..."
 
 def janken
 
-    puts "0(グー)1(チョキ)2(パー)"
+  puts "0(グー)1(チョキ)2(パー)"
 
-    player_hand = gets.to_i
+  player_hand = gets.to_i
 
-    com_hand = rand(3)
-    
-    jankens = ["グー","チョキ","パー"]
+  com_hand = rand(3)
 
-    puts "ほい"
+  jankens = ["グー","チョキ","パー"]
 
-    puts "あなたのが出したのは#{jankens[player_hand]} , 相手が出したのは#{[com_hand]}"
-    
-    drow = player_hand == com_hand
-    win = (player_hand == 0 && com_hand == 1) || (player_hand == 1 && com_hand == 2) || (player_hand == 2 && com_hand == 0)
-    #lose = !(win)
+  puts "ほい"
 
-    if drow
-        puts "あいこで"
-        return true
+  puts "あなたのが出したのは#{jankens[player_hand]} , 相手が出したのは#{jankens[com_hand]}"
+
+  drow = player_hand == com_hand
+  win = (player_hand == 0 && com_hand == 1) || (player_hand == 1 && com_hand == 2) || (player_hand == 2 && com_hand == 0)
+  lose = (player_hand == 0 && com_hand == 2) || (player_hand == 1 && com_hand == 0) || (player_hand == 2 && com_hand == 1)
+
+  if drow
+    puts "あいこで"
+    return true
         
-    elsif win
-        puts "あなたの勝ちです！"
-        puts "-----------------"
-        puts "あっち向いて〜"
-        puts "0(上)1(下)2(左)3(右)"
+  elsif win
+    puts "あなたの勝ちです！"
+    puts "-----------------"
+    puts "あっち向いて〜"
+    puts "0(上)1(下)2(左)3(右)"
         
-        player_play = gets.to_i
-        com_play = rand(4)
+    player_play = gets.to_i
+    com_play = rand(4)
 
-        plays = ["上","下","左","右"]
-        puts "ホイ"
-        puts "あなたは#{plays[player_play]}"
-        puts "相手は#{plays[com_play]}"
+    plays = ["上","下","左","右"]
+    puts "ホイ"
+    puts "あなたは#{plays[player_play]}"
+    puts "相手は#{plays[com_play]}"
 
-        if player_play == com_play
-            puts "あなたの勝ちです!"
+    if player_play == com_play
+      puts "あなたの勝ちです!"
             
-        else
-            puts "やり直しです!"
-            return true
-        end
-
-    else 
-        puts "あなたの負けです!"
-        puts "-----------------"
-        puts "あっち向いて"
-        puts "0(上)1(下)2(左)3(右)"
-        
-        player_play = gets.to_i
-        com_play = rand(4)
-
-        plays = ["上","下","左","右"]
-        puts "ホイ"
-        puts "あなたは#{plays[player_play]}"
-        puts "相手は#{plays[com_play]}"
-
-            if player_play == com_play
-                puts "あなたの負けです!"              
-            else
-                puts "やり直しです!"
-                return true
-            end
+    else
+      puts "やり直しです!"
+      return true
     end
+
+  elsif lose
+    puts "あなたの負けです!"
+    puts "-----------------"
+    puts "あっち向いて"
+    puts "0(上)1(下)2(左)3(右)"
+    
+    player_play = gets.to_i
+    com_play = rand(4)
+
+    plays = ["上","下","左","右"]
+    puts "ホイ"
+    puts "あなたは#{plays[player_play]}"
+    puts "相手は#{plays[com_play]}"
+
+      if player_play == com_play
+        puts "あなたの負けです!"              
+      else
+        puts "やり直しです!"
+        return true
+      end
+  else
+    return false
+  end
 end
 
 next_game = true
 
 while next_game
-    next_game = janken
+  next_game = janken
 end
